c7044b57e5123b695be2d2408a93695e66fb1a47
snraets <sjstearns@gmail.com>
2015-02-03 11:26:18 -0500

    Create AW_buyerChecklistApp.js

---
+++ Award-Action-Workflow/Scripts/Apps/AW_buyerChecklistApp.js
@@ -0,0 +1,38 @@
+﻿define(["jquery"
+    , variablesObjectInitiate
+], function () {
+
+    if ($("#buyerChecklistHolder").length == 1) {
+
+        retrieveBuyerChecklist({
+            func: "ll"
+            , objAction: "RunReport"
+            , objID: coreVariables.AW_CHECKLIST_SUMMARY_BUYER
+            , inputLabel1: coreVariables.wfID
+        });
+    }
+
+    function retrieveBuyerChecklist(buyerChecklistData) {
+
+        var dfd = new jQuery.Deferred();
+
+        $.ajax({
+            url: "/livelink/llisapi.dll"
+            , data: buyerChecklistData
+            , type: "POST"
+            , success: function (data) {
+                $("#buyerChecklistHolder").append(data);  
+				$("#kickbackAccordion").accordion({ collapsible: true, heightStyle: "content" });
+				$('.clsReasonDetail')[$('.clsReasonDetail').length-1].innerHTML = $('.clsReasonDetail')[$('.clsReasonDetail').length-1].innerHTML.replace(/\n/g,'<br />');  // jhn
+                dfd.resolve();
+            }
+            , error: function () {
+                alert("Buyer Checklist failed to load");
+                dfd.reject();
+            }
+        });
+
+        return dfd.promise();
+    }
+
+})
